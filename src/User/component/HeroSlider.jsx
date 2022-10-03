5adb95319937d14fdf605402c3553f1f55586c1d
NgocAh <107913738+NgocAh@users.noreply.github.com>
2022-10-03 21:26:18 +0700

    delate app.js

--- src/User/component/HeroSlider.jsx
+++ src/User/component/HeroSlider.jsx
@@ -43,3 +43,17 @@ const HeroSliderItem = (props) => (
 
 export default HeroSlider
 
+// import React from 'react'
+// import PropTypes from 'prop-types'
+
+// const HeroSlider = props => {
+//   return (
+//     <div>HeroSlider</div>
+//   )
+// }
+
+// HeroSlider.propTypes = {
+//   data: PropTypes.array.isRequired,
+// };
+
+// export default HeroSlider
