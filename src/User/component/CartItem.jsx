aa9ad3084202e2870b722ea8ff7a3e552cac700c
TrungPhan25 <82156490+TrungPhan25@users.noreply.github.com>
2022-11-13 14:22:22 +0700

    test

--- src/User/component/CartItem.jsx
+++ src/User/component/CartItem.jsx
@@ -16,7 +16,6 @@ const CartItem = props => {
     setItem(props.item);
     setQuantity(props.item.quantity);
   }, [props.item]);
-
   const updateQuantity = (opt) =>{
     if (opt === "+") {
       dispatch(updateItem({ ...item, quantity: quantity + 1 }));
